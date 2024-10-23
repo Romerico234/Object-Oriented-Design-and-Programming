4fc1475ec8a003598405aac179f5955d9c89ee00
aimablM <aimable.mugwaneza@gmail.com>
2024-10-22 21:33:32 -0400

    -Separate class files

---
+++ exercise-08/LightsOnCommand.java
@@ -0,0 +1,11 @@
+public class LightsOnCommand implements Command{
+    Light light;
+
+    public LightsOnCommand(Light light){
+        this.light = light;
+    }
+
+    public void execute(){
+        light.on();
+    }
+}
